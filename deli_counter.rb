45935b35078d8596a1c905155cbef25fa610ed6a
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 02:12:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,4 @@ def line(place_in_line)
   line_array = []
   if place_in_line.length == 0
     puts "The line is currently empty."
-  else place_in_line.each.with_index do 
+  else place_in_line.each.with_index(1) do 
