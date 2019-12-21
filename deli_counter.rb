feccb4f5ec1d8dfcc5dcb129c283bf37b7af8679
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 02:17:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,4 +7,4 @@ def line(place_in_line)
   else place_in_line.each.with_index(1) do |index, name|
     line_array.push ("#{index}. #{name}")
   end
-  puts "The line is currently: #"
+  puts "The line is currently: #{"
