577cbc9bcbafdf679a77194bc2fba77dd4eb4481
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 02:26:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,7 @@
 katz_deli = [] 
 
-def line(place_in_line)
-  line_array = []
-  if place_in_line.length == 0
-    puts "The line is currently empty."
-  else place_in_line.each.with_index(1) do |index, name|
-    line_array.push ("#{index}. #{name}")
-  end
-  puts "The line is currently: #{line_array.join("")}"
-end
-end
+
+
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
