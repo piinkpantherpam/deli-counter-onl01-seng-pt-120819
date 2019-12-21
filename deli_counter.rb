793af8e1503e7c6524f305d307bcea1e419cd633
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 02:20:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,17 @@ def line(place_in_line)
   puts "The line is currently: #{line_array.join("")}"
 end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
